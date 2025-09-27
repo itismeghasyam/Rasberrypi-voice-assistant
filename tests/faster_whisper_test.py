fdfda26c8261a5cb3217d4ad0c8ce7994b0baee4
Kushal2205a <introvertedspacemine2@gmail.com>
2025-09-27 12:38:45 +0530

    tiny

--- tests/faster_whisper_test.py
+++ tests/faster_whisper_test.py
@@ -5,6 +5,6 @@ client = OpenAI(api_key="cant-be-empty", base_url="http://localhost:8000/v1/")
 
 audio_file = open(os.path.expanduser("~/Rasberrypi-voice-assistant/recorded.wav"), "rb")
 transcript = client.audio.transcriptions.create(
-    model="Systran/faster-distil-whisper-large-v3", file=audio_file
+    model="Systran/faster-distil-whisper-tiny-v3", file=audio_file
 )
 print(transcript.text)
