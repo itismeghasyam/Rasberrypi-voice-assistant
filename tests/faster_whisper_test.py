4d26f2ee07f9a1c9bc3b20e862b159cefcc3ce31
Kushal2205a <introvertedspacemine2@gmail.com>
2025-09-27 12:35:19 +0530

    fixed bug

--- tests/faster_whisper_test.py
+++ tests/faster_whisper_test.py
@@ -1,8 +1,9 @@
 from openai import OpenAI
+import os 
 
 client = OpenAI(api_key="cant-be-empty", base_url="http://localhost:8000/v1/")
 
-audio_file = open("~/Rasberrypi-voice-assistant/recorded.wav", "rb")
+audio_file = open(os.path.expanduser("~/Rasberrypi-voice-assistant/recorded.wav"), "rb")
 transcript = client.audio.transcriptions.create(
     model="Systran/faster-distil-whisper-large-v3", file=audio_file
 )
